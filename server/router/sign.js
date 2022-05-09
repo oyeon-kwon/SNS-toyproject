8118ba65829f592c09e2aa6bcf71005711f68bf1
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-05-09 15:15:55 +0900

    Delete: 서버 폴더 삭제

--- server/router/sign.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router(); 
-const sign = require("../controllers/sign");
-
-router.post("/signin", sign.signin);
-router.post("/login", sign.login);
-router.delete("/logout", sign.logout);
-
-module.exports = router;
